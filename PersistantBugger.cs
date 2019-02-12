9b899b1e6824df96dfd9222411d88f9f202cfe4c
Avinash Singh <47045062+avinashsinghdhillon@users.noreply.github.com>
2019-02-12 11:46:34 -0600

    Add files via upload

---
+++ PersistantBugger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistentBugger
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Result: " + Persistence(999));
+            Console.ReadKey();
+        }
+        public static int Persistence(long n)
+        {
+            int cnt = 0;
+            int result = 1;
+            List<int> dList = new List<int>();
+            dList = addDigits(n);
+
+            while (dList.Count > 1)
+            {
+                cnt++;
+                result = 1;
+                foreach (int i in dList)
+                {
+                    result = result * i;
+                }
+                dList.Clear();
+                dList = addDigits(result);
+            }
+
+            return cnt;
+        }
+
+        public static List<int> addDigits(long num)
+        {
+            List<int> lst = new List<int>();
+            while (num > 0)
+            {
+                lst.Add((int)(num % 10));
+                num = num / 10;
+            }
+            return lst;
+        }
+    }
+}
---
+++ RGBToHexConversion.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGBToHexConversion
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("result: " + Rgb(255, 255, 300));
+            Console.ReadKey();
+
+        }
+
+        public static string Rgb(int r, int g, int b)
+        {
+            string result = "";
+            List<int> rgb = new List<int>();
+            rgb.Add(r);
+            rgb.Add(g);
+            rgb.Add(b);
+
+            foreach(int i in rgb)
+            {
+                string temp1, temp2;
+                //convert each integer into Hex and add to result
+                if (i >= 0)
+                {
+                    if(i < 16)
+                    {
+                        temp1 = "0";
+                    }
+                    else if (i / 16 < 10)
+                    {
+                        temp1 = (i / 16).ToString();
+                    }
+                    else if (i / 16 > 15)
+                    {
+                        temp1 = Alphabet(15);
+                    }
+                    else
+                    {
+                        temp1 = Alphabet(i / 16);
+                    }
+                    if( i / 16 > 15)
+                    {
+                        temp2 = Alphabet(15);
+                    }
+                    else if (i % 16 < 10)
+                    {
+                        temp2 = (i % 16).ToString();
+                    }
+                    else if(i < 16) // single digit number
+                    {
+                        temp2 = (Alphabet(i));
+                    }
+                    else
+                    {
+                        temp2 = Alphabet(i % 16);
+                    }
+                    result += temp1 + temp2;
+                }
+                else
+                {
+                    result += "00";
+                }
+            }
+
+            return result;
+        }
+        private static string Alphabet(int n)
+        {
+            switch (n)
+            {
+                case 10:
+                    return "A";
+                case 11:
+                    return "B";
+                case 12:
+                    return "C";
+                case 13:
+                    return "D";
+                case 14:
+                    return "E";
+                case 15:
+                    return "F";
+                default:
+                    return n.ToString();
+
+            }
+        }
+
+    }
+}
