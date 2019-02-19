20e505c5c6823d6d3ec6358b9f0cedb0d12241cf
Avinash Singh <47045062+avinashsinghdhillon@users.noreply.github.com>
2019-02-19 10:01:18 -0600

    Add files via upload

---
+++ Digital Root.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            long numb = 123123;
+            Number num = new Number();
+            Console.WriteLine("Number: " + numb + ", Digital Root: " + num.DigitalRoot(numb).ToString());
+            Console.ReadKey();
+        }
+
+        
+    }
+    public class Number
+    {
+        public int DigitalRoot(long n)
+        {
+            return (int)(1 + (n - 1) % 9);
+        }
+    }
+}
