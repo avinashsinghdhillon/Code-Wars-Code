5c554ede825c7be0c590220b3afe157cede16389
Avinash Singh <47045062+avinashsinghdhillon@users.noreply.github.com>
2019-02-21 20:16:19 -0600

    Add files via upload

---
+++ Meeting_NameManipulation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meeting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Result: {0}", Meeting("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn"));
+            Console.ReadKey();
+        }
+
+        public static string Meeting(string s)
+        {
+            string result = "";
+            List<string> lst = s.Split(';').ToList();
+            string[] names = { };
+            for (int i = 0; i < lst.Count; i++)
+            {
+                names = lst[i].Split(':');
+                lst[i] = "(" + names[1].ToUpper() + ", " + names[0].ToUpper() + ")";
+            }
+
+            lst.Sort();
+
+            result = string.Join("", lst.ToArray());
+
+            return result;
+        }
+    }
+}
